c15cdce0516febc02452a33bf67bc94ce0bb2f33
Rifa9oct <kohinuraktherr@gmail.com>
2024-04-13 20:07:08 +0600

    create patch and delete api endpoint

--- app/api/movie/[id]/route.js
+++ app/api/movie/[id]/route.js
@@ -7,3 +7,20 @@ export async function GET(_request, { params }) {
     return Response.json(movie);
 }
 
+export async function PATCH(request, { params }) {
+    const movie = await request.json();
+    const movieId = parseInt(params.id);
+    const movieIndex = movieList.results.findIndex(movie => movie.id === movieId);
+    movieList.results[movieIndex].title = movie.title;
+
+    return Response.json(movieList.results[movieIndex]);
+}
+
+export async function DELETE(_request, { params }) {
+    const movieId = parseInt(params.id);
+    const movieIndex = movieList.results.findIndex(movie => movie.id === movieId);
+    const movieToDelete = movieList.results[movieIndex];
+    movieList.results.splice(movieIndex, 1);
+
+    return Response.json(movieToDelete);
+}
